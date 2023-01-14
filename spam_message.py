9695375b7ae16176af8fcd205fa53c57777faae1
QadeerAhamd <qadeerahmad294gb@gmail.com>
2023-01-14 21:25:53 +0500

    Added function to all code and call the function

--- spam_message.py
+++ spam_message.py
@@ -2,10 +2,13 @@
 import pyautogui
 import time
 
-time.sleep(3)
-with open('spam_message_text.txt', 'r') as file:
-    a = file.readlines()
-    for lines in a:
-        pyautogui.write(lines)
-        pyautogui.press('enter')
-        time.sleep(0.1)
+def main():
+    time.sleep(3)
+    with open('spam_message_text.txt', 'r') as file:
+        a = file.readlines()
+        for lines in a:
+            pyautogui.write(lines)
+            pyautogui.press('enter')
+            time.sleep(0.1)
+
+main()
